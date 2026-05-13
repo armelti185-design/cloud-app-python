27e8c77ec7cebbc596898149c41debc38aa66589
Tié Armel Aimé <armelti185@gmail.com>
2026-05-13 16:10:17 +0200

    version finale sans flask

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-app = Flask(__name__)
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("Application deployee sur Render sans Flask 🚀")
 
-@app.route('/')
-def home():
-    return "Bienvenue sur mon application Python deployée sur le Cloud"
+port = int(os.environ.get("PORT", 10000))  # Render fournit PORT
 
-if __name__ == '__main__':
-    app.run()
+server = HTTPServer(("0.0.0.0", port), Handler)
+
+print(f"Server running on port {port}")
+server.serve_forever()
